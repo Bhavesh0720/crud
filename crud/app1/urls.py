bbbf19248f798b47745bd130e78fa7ddaf5814dc
Bhavesh0720 <bhavesh0720@gmail.com>
2025-10-10 12:12:40 +0530

    added editing and deleting employee functionality

--- crud/app1/urls.py
+++ crud/app1/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.crud, name='crud'),
     path('add_emp', views.add_emp, name='add_emp'),
+    path('edit_employee/<int:id>', views.edit_employee, name='edit_employee'),
+    path('delete_employee/<int:id>', views.delete_employee, name='delete_employee'),
 ]
 
--- crud/app1/views.py
+++ crud/app1/views.py
@@ -29,3 +29,25 @@ def add_emp(request):
         return render(request, 'crud.html', {'msg':msg})
 
     return redirect('crud')
+
+def edit_employee(request, id):
+    emp = Employee.objects.get(id=id)
+    if request.POST:
+        name = request.POST['name']
+        email = request.POST['email']
+        address = request.POST['address']
+        phone = request.POST['phone']
+
+        emp.name=name
+        emp.email=email
+        emp.address=address,
+        emp.phone=phone
+
+        emp.save()
+        return redirect('crud')    
+    return render(request, 'crud.html', {'emp':emp})
+
+def delete_employee(request, id):
+    emp = Employee.objects.get(id=id)
+    emp.delete()
+    return redirect('crud')
